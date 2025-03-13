520dcb4cab23430474912624bc87e174916f6f7c
KaijuuYahoo <muhammad.rasyid.ft24@mail.umy.ac.id>
2025-03-13 16:29:30 +0700

    procedure bubblesort

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,2 +1,55 @@
 #include <iostream>
-using namespace std
+using namespace std;
+
+int arr[15];
+int n; //input jumlah data
+
+void inputData(){
+    while(true)
+    {
+        cout << "Masukkan Jumlah Data : ";
+        cin >> n;
+
+        if (n < 15){
+            break;
+        }
+        else{
+           cout << " Jumlah Data Maksimal 15 " << endl;
+        }
+
+    }
+    cout << " ======================= " <<endl ;
+    cout << " =====Masukkan Data===== " << endl;
+    cout << " ======================= ";
+    cout << endl ;
+
+    for (int i =0; i < n ; i++){
+        cout << " Masukkan Data ke-" << i + 1 << " : ";
+        cin >> arr[i] ;
+    }
+}
+
+void BubbleSort (){
+    int pass = 1;//Step 1
+    do
+    {
+        for (int j = 0; j <= n - 1 - pass; j++)//Step 2
+        {
+            if (arr[j] > arr[j+1]) //Step 3
+            {
+                int temp;
+                temp = arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        } 
+        pass = pass + 1; //Step 4
+    } while (pass <= n-1); //Step 5
+    
+}
+
+ int main (){
+    inputData();
+    BubbleSort();
+
+ }
